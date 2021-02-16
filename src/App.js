cb528653eec7366e23c9212322dfb5f043edebfd
reinaldoriant <reinaldoriant@gmail.com>
2021-02-16 17:55:07 +0700

    add design review section

--- src/App.js
+++ src/App.js
@@ -1,74 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="Parentbox">
-      <FotoProduk/>
-      <ProdukInfo isDiscount="yes" name="Adidas Yeezy Boost 350 V2 Cinder" category="Lifestyle"/>
+      <FotoProduk />
+      <ProdukInfo isDiscount="yes" name="Adidas Yeezy Boost 350 V2 Cinder" category="Lifestyle" />
+
+      <div className="Review-box">
+        <h2>Reviews</h2>
+        <div className="Item">
+          <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="user" />
+          <div className="User">
+            <h3>Benedetta</h3>
+            <p>Sepatunya keren banget nih seleraku</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-function FotoProduk(){
-  return(
+function FotoProduk() {
+  return (
     <div className="Foto">
-      <img src="yeezy-white.jpg" alt="shoes"/>
+      <img src="yeezy-black.png" alt="shoes" />
     </div>
   );
 }
 
-function CheckDiscount(props){
+function CheckDiscount(props) {
   const { isDiscount } = props;
-  if(isDiscount === "yes"){
+  if (isDiscount === "yes") {
     return (
       <p>Diskon 50% off</p>
     );
   }
-  else if(isDiscount ==="coming") {
-    return(
+  else if (isDiscount === "coming") {
+    return (
       <p>Akan ada diskon ...</p>
     );
   }
-  else
-  {
+  else {
     return (
       <p>Belum ada diskon</p>
     );
   }
 }
 
-function ProdukInfo(proops){
-  const {category, name, isDiscount} =proops;
+function ProdukInfo(proops) {
+  const { category, name, isDiscount } = proops;
   const price = 320;
-  const benefits = ["Tidak kusut terkena air", "Bahan lebih halus", "Bahan lentur","Tahan lama", "Bahan kuat", "Original 100%"];
-  const listBenefits = benefits.map((itemBenefit)=>
+  const benefits = ["Tidak kusut terkena air", "Bahan lebih halus", "Bahan lentur", "Tahan lama", "Bahan kuat", "Original 100%"];
+  const listBenefits = benefits.map((itemBenefit) =>
     <li key={itemBenefit}>{itemBenefit}</li>
   );
-   return (
+  return (
     <div>
       <div className="Deskripsi">
         <p className="Cate">{category}</p>
         <h1 className="Tittle">{name}</h1>
         <p className="Price">$ {price}</p>
-        <CheckDiscount isDiscount={isDiscount}/>
+        <CheckDiscount isDiscount={isDiscount} />
         <p className="Info">
-          Yeezy adds another colorway of its most popular design with the adidas Yeezy 350 Cinder, now available on Shoes Room. 
-          This 350 V2 stays true to Yeezy’s signature palette, sporting dark earth tones that provide a natural feeling. 
-          This Yeezy 350 V2 is composed of a cinder Primeknit upper with a tonal side stripe. 
+          Yeezy adds another colorway of its most popular design with the adidas Yeezy 350 Cinder, now available on Shoes Room.
+          This 350 V2 stays true to Yeezy’s signature palette, sporting dark earth tones that provide a natural feeling.
+          This Yeezy 350 V2 is composed of a cinder Primeknit upper with a tonal side stripe.
           A cinder Boost cushioned midsole sole and gold outsole completes the design. These sneakers released in March of 2020 and retailed for $220.
         </p>
         <ul>
           {listBenefits}
         </ul>
-        <a onClick={(e) =>TambahCart(name,e)} href="#addcart">Add to Cart</a>
+        <a onClick={(e) => TambahCart(name, e)} href="#addcart">Add to Cart</a>
       </div>
     </div>
-    );
+  );
 }
 
-function TambahCart(e){
-  return console.log("Membeli Produk ..." +e)
+function TambahCart(e) {
+  return console.log("Membeli Produk ..." + e)
 }
 export default App;
