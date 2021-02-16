72885fb8ab2f501e8df586d891b13c85585945d7
reinaldoriant <reinaldoriant@gmail.com>
2021-02-16 17:18:40 +0700

    add map array

--- src/App.js
+++ src/App.js
@@ -42,6 +42,10 @@ function CheckDiscount(props){
 function ProdukInfo(proops){
   const {category, name, isDiscount} =proops;
   const price = 320;
+  const benefits = ["Tidak kusut terkena air", "Bahan lebih halus", "Bahan lentur","Tahan lama", "Bahan kuat", "Original 100%"];
+  const listBenefits = benefits.map((itemBenefit)=>
+    <li key={itemBenefit}>{itemBenefit}</li>
+  );
    return (
     <div>
       <div className="Deskripsi">
@@ -55,6 +59,9 @@ function ProdukInfo(proops){
           This Yeezy 350 V2 is composed of a cinder Primeknit upper with a tonal side stripe. 
           A cinder Boost cushioned midsole sole and gold outsole completes the design. These sneakers released in March of 2020 and retailed for $220.
         </p>
+        <ul>
+          {listBenefits}
+        </ul>
         <a onClick={(e) =>TambahCart(name,e)} href="#addcart">Add to Cart</a>
       </div>
     </div>
